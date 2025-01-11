fa99e31942aa55045b4a6761d1c73ac720d2d4ba
Ali <hemati@activecleaners.ir>
2025-01-11 14:33:59 +0330

    Implement  ProtoFileProvider, ExceptionInterceptor.

---
+++ ClientServer-gRPC.gRPC/Interceptors/ExceptionInterceptor.cs
@@ -0,0 +1,97 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+
+namespace Interceptors;
+
+/// <summary>
+///     Interceptor to handle exceptions for gRPC methods.
+/// </summary>
+public class ExceptionInterceptor : Interceptor
+{
+    /// <inheritdoc />
+    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
+        TRequest request,
+        ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation)
+    {
+        try
+        {
+            return await continuation(request, context);
+        }
+        catch (Exception ex)
+        {
+            ExceptionHandle<TRequest, TResponse>(ex);
+            throw;
+        }
+    }
+
+    /// <inheritdoc />
+    public override async Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        ServerCallContext context,
+        ClientStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        try
+        {
+            return await continuation(requestStream, context);
+        }
+        catch (Exception ex)
+        {
+            ExceptionHandle<TRequest, TResponse>(ex);
+            throw;
+        }
+    }
+
+    /// <inheritdoc />
+    public override async Task ServerStreamingServerHandler<TRequest, TResponse>(
+        TRequest request,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        ServerStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        try
+        {
+            await continuation(request, responseStream, context);
+        }
+        catch (Exception ex)
+        {
+            ExceptionHandle<TRequest, TResponse>(ex);
+            throw;
+        }
+    }
+
+    /// <inheritdoc />
+    public override async Task DuplexStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        DuplexStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        try
+        {
+            await continuation(requestStream, responseStream, context);
+        }
+        catch (Exception ex)
+        {
+            ExceptionHandle<TRequest, TResponse>(ex);
+            throw;
+        }
+    }
+
+    /// <summary>
+    ///     Handles exceptions by adding metadata and throwing an RpcException.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the gRPC request.</typeparam>
+    /// <typeparam name="TResponse">The type of the gRPC response.</typeparam>
+    /// <param name="ex">The exception to handle.</param>
+    private static void ExceptionHandle<TRequest, TResponse>(Exception ex)
+    {
+        var correlationId = Guid.NewGuid().ToString();
+        Metadata trailers = new Metadata
+        {
+            { "CorrelationId", correlationId },
+            { "Interceptor", "True" }
+        };
+        throw new RpcException(new Status(StatusCode.Internal, ex.Message), trailers, "Server Side Error");
+    }
+}   
--- ClientServer-gRPC.gRPC/Program.cs
+++ ClientServer-gRPC.gRPC/Program.cs
@@ -1,14 +1,48 @@
+using ClientServer_gRPC.BLL.Services;
+using ClientServer_gRPC.DAL;
+using ClientServer_gRPC.DAL.Repositories;
+using ClientServer_gRPC.Domain.Repositories;
+using ClientServer_gRPC.Domain.Services;
 using ClientServer_gRPC.gRPC.Services;
+using GrpcInfrastructures;
+using Interceptors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc(options =>
+{
+    options.EnableDetailedErrors = true;
+    options.Interceptors.Add<ExceptionInterceptor>();
+});
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddDbContext<StudentDbContext>();
+builder.Services.AddSingleton<ProtoFileProvider>();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
+app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+
+// Config minimal Apis
+app.MapGet("/protos", (ProtoFileProvider protoFileProvider) => Results.Ok((object?)protoFileProvider.GetAll()));
+app.MapGet("/protos/v{version:int}/{protoName}",
+    (ProtoFileProvider protoFileProvider, int version, string protoName) =>
+    {
+        string filePath = protoFileProvider.GetPath(version, protoName);
+        return string.IsNullOrWhiteSpace(filePath) ? Results.NotFound() : Results.Ok(filePath);
+    });
+app.MapGet("/protos/v{version:int}/{protoName}/view",
+   async (ProtoFileProvider protoFileProvider, int version, string protoName) =>
+   {
+       string fileContent = await protoFileProvider.GetContent(version, protoName);
+       return string.IsNullOrWhiteSpace(fileContent) ? Results.NotFound() : Results.Text(fileContent);
+   });
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
